9959e56b993c3a9f9048a8d8e4da9ad8e58e1b1f
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 14:34:45 +0530

    UC2 Added Calculate daily employee wage

--- src/com/bl_java_rfp/programming_constructs/EmployeeWageComputation.java
+++ src/com/bl_java_rfp/programming_constructs/EmployeeWageComputation.java
@@ -7,14 +7,20 @@ public class EmployeeWageComputation {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOURS = 8;
+
+        int dailyWage = 0;
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 
         if (empCheck == IS_FULL_TIME) {
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
+        System.out.println("Daily Employee Wage: " + dailyWage);
+
     }
 }
-
