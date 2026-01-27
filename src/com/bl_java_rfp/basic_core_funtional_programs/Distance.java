5a31f2ee3a8db7854669c294ec12234a7baf6de2
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 17:41:47 +0530

    Added Basic Functional Programs

---
+++ src/com/bl_java_rfp/basic_core_funtional_programs/Distance.java
@@ -0,0 +1,11 @@
+package com.bl_java_rfp.basic_core_funtional_programs;
+
+public class Distance {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+
+        double distance = Math.sqrt(x * x + y * y);
+        System.out.println("Euclidean Distance: " + distance);
+    }
+}
