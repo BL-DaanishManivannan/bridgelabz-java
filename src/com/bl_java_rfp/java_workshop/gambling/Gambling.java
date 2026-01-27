1cb47a3a96a3e5318278a456fa17d5836ab53b90
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 20:01:52 +0530

    Add: UC1 initialize stake and bet

--- src/com/bl_java_rfp/java_workshop/gambling/Gambling.java
+++ src/com/bl_java_rfp/java_workshop/gambling/Gambling.java
@@ -1,4 +1,14 @@
 package com.bl_java_rfp.java_workshop.gambling;
 
 public class Gambling {
+    static final int STAKE = 100;
+    static final int BET = 1;
+
+    public static void main(String[] args) {
+        startGambling();
+    }
+
+    static void startGambling() {
+        int stake = STAKE;
+    }
 }
