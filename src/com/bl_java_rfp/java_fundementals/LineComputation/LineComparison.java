854408cdab7cd975da3348f03348327173a87b6e
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 12:59:21 +0530

    UC3  Fix Compare two lines using compareTo method

--- src/com/bl_java_rfp/java_fundementals/LineComputation/LineComparison.java
+++ src/com/bl_java_rfp/java_fundementals/LineComputation/LineComparison.java
@@ -7,10 +7,10 @@ public class LineComparison {
         int x1 = 2, y1 = 4;
         int x2 = 6, y2 = 8;
 
-        double line1Length = Math.sqrt(
+        Double line1Length = Math.sqrt(
                 Math.pow(4 - 2, 2) + Math.pow(6 - 4, 2)
         );
-        double line2Length = Math.sqrt(
+        Double line2Length = Math.sqrt(
                 Math.pow(8 - 6, 2) + Math.pow(10 - 8, 2)
         );
         int result = line1Length.compareTo(line2Length);
