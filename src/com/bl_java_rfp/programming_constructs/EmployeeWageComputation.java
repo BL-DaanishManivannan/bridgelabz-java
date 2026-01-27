4a1a316ce9960ecdb50a03b3a2d178b5883648ab
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 14:38:11 +0530

    UC3 Added  Add part-time employee wage

--- src/com/bl_java_rfp/programming_constructs/EmployeeWageComputation.java
+++ src/com/bl_java_rfp/programming_constructs/EmployeeWageComputation.java
@@ -10,13 +10,22 @@ public class EmployeeWageComputation {
         int WAGE_PER_HOUR = 20;
         int FULL_DAY_HOURS = 8;
 
+        int IS_ABSENT = 0;
+        int IS_PART_TIME = 2;
+
+        int PART_TIME_HOURS = 4;
+
         int dailyWage = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
 
         if (empCheck == IS_FULL_TIME) {
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
             System.out.println("Employee is Present");
+        } else if (empCheck == IS_PART_TIME) {
+            dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            System.out.println("Employee is Part Time");
         } else {
             System.out.println("Employee is Absent");
         }
