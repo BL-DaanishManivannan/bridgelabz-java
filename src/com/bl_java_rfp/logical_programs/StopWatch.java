f38daf035e1fd5bb8fb67f6570c842407829ab50
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 17:52:15 +0530

    Added Logical Programs

---
+++ src/com/bl_java_rfp/logical_programs/StopWatch.java
@@ -0,0 +1,25 @@
+package com.bl_java_rfp.logical_programs;
+
+import java.util.Scanner;
+
+public class StopWatch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Press Enter to start the Stopwatch");
+        sc.nextLine();
+
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("Press Enter to stop the Stopwatch");
+        sc.nextLine();
+
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Elapsed Time: " + elapsedTime + " milliseconds");
+    }
+}
+
