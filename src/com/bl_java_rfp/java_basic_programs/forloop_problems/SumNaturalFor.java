2e5908fd5b23a930ba9a3c2d35ca1c272ec13c70
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 10:31:09 +0530

    Added Programs in FOR LOOP

---
+++ src/com/bl_java_rfp/java_basic_programs/forloop_problems/SumNaturalFor.java
@@ -0,0 +1,18 @@
+package com.bl_java_rfp.java_basic_programs.forloop_problems;
+
+import java.util.Scanner;
+
+public class SumNaturalFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
