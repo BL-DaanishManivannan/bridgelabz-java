4631220dc26217a73bbba8b6f497e2330f214ebd
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 12:37:34 +0530

    UC4 Added  Refactor employee wage calculation using switch case

--- src/com/bl_java_rfp/java_fundementals/EmployeeWage/EmployeeWageComputation.java
+++ src/com/bl_java_rfp/java_fundementals/EmployeeWage/EmployeeWageComputation.java
@@ -1,9 +1,9 @@
 package com.bl_java_rfp.java_fundementals.EmployeeWage;
-
 public class EmployeeWageComputation {
 
     private static final int IS_ABSENT = 0;
     private static final int IS_PRESENT = 1;
+    private static final int IS_FULL_TIME = 1;
 
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOURS = 8;
@@ -19,15 +19,19 @@ public class EmployeeWageComputation {
 
         int dailyWage = 0;
 
-        if (attendance == IS_PRESENT) {
-            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
-            System.out.println("Employee is Present");
-        } else if (attendance == IS_PART_TIME) {
-            dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
-            System.out.println("Employee is Part Time");
-        } else {
-            System.out.println("Employee is Absent");
+        switch (attendance) {
+            case IS_FULL_TIME:
+                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+                System.out.println("Employee is Full Time");
+                break;
+            case IS_PART_TIME:
+                dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+                System.out.println("Employee is Part Time");
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
+
         System.out.println("Daily Employee Wage: " + dailyWage);
     }
 }
