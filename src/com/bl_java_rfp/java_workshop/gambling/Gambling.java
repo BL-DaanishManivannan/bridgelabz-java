92afea7abf08b3f71e07f32f128201b2ddfa821c
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 20:02:56 +0530

    Add: UC2 win or lose one dollar per bet

--- src/com/bl_java_rfp/java_workshop/gambling/Gambling.java
+++ src/com/bl_java_rfp/java_workshop/gambling/Gambling.java
@@ -1,8 +1,10 @@
 package com.bl_java_rfp.java_workshop.gambling;
 
+import java.util.Random;
 public class Gambling {
     static final int STAKE = 100;
     static final int BET = 1;
+    static final int WIN = 1;
 
     public static void main(String[] args) {
         startGambling();
@@ -10,5 +12,18 @@ public class Gambling {
 
     static void startGambling() {
         int stake = STAKE;
+        stake = playGame(stake);
+    }
+
+    static int playGame(int stake) {
+        Random random = new Random();
+        int result = random.nextInt(2);
+
+        if (result == WIN) {
+            stake += BET;
+        } else {
+            stake -= BET;
+        }
+        return stake;
     }
 }
