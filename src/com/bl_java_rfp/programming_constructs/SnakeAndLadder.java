0567698fd853a04b51e4181864d0157b41261a69
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 15:09:20 +0530

    Initial Code Added

---
+++ src/com/bl_java_rfp/programming_constructs/SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bl_java_rfp.programming_constructs;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake and Ladder Game");
+
+        int position = 0;
+
+        System.out.println("Player starts at position: " + position);
+    }
+}
