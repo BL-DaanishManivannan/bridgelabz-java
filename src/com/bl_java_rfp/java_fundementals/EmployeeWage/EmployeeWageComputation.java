f0b917b0d952e21eb7fb4f999dfface0b0589b4c
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-27 12:31:28 +0530

    UC Added Support part-time employee wage calculation

--- src/com/bl_java_rfp/java_fundementals/EmployeeWage/EmployeeWageComputation.java
+++ src/com/bl_java_rfp/java_fundementals/EmployeeWage/EmployeeWageComputation.java
@@ -8,17 +8,23 @@ public class EmployeeWageComputation {
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOURS = 8;
 
+    private static final int IS_PART_TIME = 2;
+    private static final int PART_TIME_HOURS = 4;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int attendance = (int) (Math.random() * 2);
+        int attendance = (int) (Math.random() * 3);
 
         int dailyWage = 0;
 
         if (attendance == IS_PRESENT) {
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
             System.out.println("Employee is Present");
+        } else if (attendance == IS_PART_TIME) {
+            dailyWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+            System.out.println("Employee is Part Time");
         } else {
             System.out.println("Employee is Absent");
         }
