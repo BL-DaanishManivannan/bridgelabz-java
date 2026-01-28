64f99db95387b1fb1a32bf90e6c3798df9a98361
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 14:58:29 +0530

    Add UC1: Create Account model with balance

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/Account/Account.java
@@ -0,0 +1,10 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.Account;
+
+public class Account {
+
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+}
