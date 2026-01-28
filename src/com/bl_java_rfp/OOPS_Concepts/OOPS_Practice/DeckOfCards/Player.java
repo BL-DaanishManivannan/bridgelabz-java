8aea544b6b09edef5b3e66f786fd8a77621711a8
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 17:33:31 +0530

    Add UC5.4: Sort player cards by rank without using collections

--- src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/Player.java
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/Player.java
@@ -7,6 +7,11 @@ public class Player {
     private CardNode tail;
     private int cardCount;
 
+    private static final String[] RANK_ORDER = {
+            "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "Jack", "Queen", "King", "Ace"
+    };
+
     public Player(String playerName) {
         this.playerName = playerName;
         this.head = null;
@@ -14,7 +19,6 @@ public class Player {
         this.cardCount = 0;
     }
 
-    // UC5.3: Add card to linked list
     public void addCard(String card) {
         CardNode newNode = new CardNode(card);
 
@@ -28,15 +32,46 @@ public class Player {
         cardCount++;
     }
 
+    // UC5.4: Sort cards by rank
+    public void sortCardsByRank() {
+
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        boolean swapped;
+        do {
+            swapped = false;
+            CardNode current = head;
+
+            while (current.next != null) {
+                if (getRankIndex(current.card) > getRankIndex(current.next.card)) {
+                    String temp = current.card;
+                    current.card = current.next.card;
+                    current.next.card = temp;
+                    swapped = true;
+                }
+                current = current.next;
+            }
+        } while (swapped);
+    }
+
+    private int getRankIndex(String card) {
+        String rank = card.split(" ")[0];
+
+        for (int i = 0; i < RANK_ORDER.length; i++) {
+            if (RANK_ORDER[i].equals(rank)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public String getPlayerName() {
         return playerName;
     }
 
     public CardNode getHead() {
         return head;
-    }
-
-    public int getCardCount() {
-        return cardCount;
     }
 }
