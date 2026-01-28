d38c143cef6740dc9348e8d4b1473774a239df66
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 15:02:40 +0530

    Added Data to test

--- src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/Account/AccountTest.java
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/Account/AccountTest.java
@@ -3,6 +3,13 @@ package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.Account;
 public class AccountTest {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to Account Debit Validation Program");
+
+        Account account = new Account(5000);
+
+        account.debit(2000);
+
+        account.debit(4000);
     }
 }
