c88825aacd69ca990d90f114fa9d6af7dd722919
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 15:28:38 +0530

    Add UC0: Display welcome message for commercial data processing

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/Commercial/CommercialMain.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.Commercial;
+
+public class CommercialMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Commercial Data Processing Program");
+    }
+}
