2260822e79d9e856331d2ad728a210bbc2093945
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 12:09:24 +0530

    Program Check - Added Necessary Data and Called Class Methods from AddressBookMain

--- src/com/bl_java_rfp/OOPS_Concepts/AddressBook/AddressBookMain.java
+++ src/com/bl_java_rfp/OOPS_Concepts/AddressBook/AddressBookMain.java
@@ -4,5 +4,32 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        AddressBook addressBook = new AddressBook();
+
+        ContactPerson person = new ContactPerson(
+                "John",
+                "Doe",
+                "123 Street",
+                "Chennai",
+                "Tamil Nadu",
+                600001,
+                9876543210L,
+                "john.doe@email.com"
+        );
+
+        addressBook.addContact(person);
+
+        addressBook.editContact(
+                "John",
+                "456 New Street",
+                "Bangalore",
+                "Karnataka",
+                560001,
+                9123456789L,
+                "john.updated@email.com"
+        );
+
+        addressBook.deleteContact("John");
     }
 }
