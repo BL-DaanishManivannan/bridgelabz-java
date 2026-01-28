0462df2a1b3963cb8684209837cc9644ac6308be
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 14:29:23 +0530

    Add UC1: Create Stock Class  with basic attributes

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/StockMarket/Stock.java
@@ -0,0 +1,14 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.StockMarket;
+
+public class Stock {
+
+    private String stockName;
+    private int numberOfShares;
+    private double sharePrice;
+
+    public Stock(String stockName, int numberOfShares, double sharePrice) {
+        this.stockName = stockName;
+        this.numberOfShares = numberOfShares;
+        this.sharePrice = sharePrice;
+    }
+}
