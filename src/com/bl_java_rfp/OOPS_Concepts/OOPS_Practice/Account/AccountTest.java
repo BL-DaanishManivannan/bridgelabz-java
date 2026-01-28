8f435fa067ca66901a287c85c88527286540117e
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 14:57:11 +0530

    Add UC0: Display welcome message for account debit validation

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/Account/AccountTest.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.Account;
+
+public class AccountTest {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Account Debit Validation Program");
+    }
+}
