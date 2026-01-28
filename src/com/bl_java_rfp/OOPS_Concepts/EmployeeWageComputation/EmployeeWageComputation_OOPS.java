803260b434735679209661ed867fc2f017c19449
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 13:59:23 +0530

    UC8 Added parameter Based Method for EWC

--- src/com/bl_java_rfp/OOPS_Concepts/EmployeeWageComputation/EmployeeWageComputation_OOPS.java
+++ src/com/bl_java_rfp/OOPS_Concepts/EmployeeWageComputation/EmployeeWageComputation_OOPS.java
@@ -6,81 +6,55 @@ public class EmployeeWageComputation_OOPS {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-    private final String companyName;
-    private final int wagePerHour;
-    private final int maxWorkingDays;
-    private final int maxWorkingHours;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
 
-    private int totalEmployeeWage;
+    public static void main(String[] args) {
 
+        System.out.println("Welcome to Employee Wage Computation Program");
 
-    public EmployeeWageComputation_OOPS(String companyName,
-                                   int wagePerHour,
-                                   int maxWorkingDays,
-                                   int maxWorkingHours) {
-
-        this.companyName = companyName;
-        this.wagePerHour = wagePerHour;
-        this.maxWorkingDays = maxWorkingDays;
-        this.maxWorkingHours = maxWorkingHours;
+        computeEmployeeWage("TCS", 20, 20, 100);
+        computeEmployeeWage("Infosys", 25, 22, 110);
+        computeEmployeeWage("Wipro", 18, 20, 90);
     }
 
-    private int checkAttendance() {
-        return (int) (Math.floor(Math.random() * 10) % 3);
-    }
-
-    private int getDailyWorkingHours(int empType) {
-
-        switch (empType) {
-            case IS_FULL_TIME:
-                return 8;
-
-            case IS_PART_TIME:
-                return 4;
-
-            default:
-                return 0;
-        }
-    }
-
-    public void computeEmployeeWage() {
+    // UC8: Compute wage using method parameters (NO class variables)
+    public static void computeEmployeeWage(String companyName,
+                                           int wagePerHour,
+                                           int maxWorkingDays,
+                                           int maxWorkingHours) {
 
         int totalWorkingDays = 0;
         int totalWorkingHours = 0;
+        int totalEmployeeWage = 0;
 
         while (totalWorkingDays < maxWorkingDays
                 && totalWorkingHours < maxWorkingHours) {
 
             totalWorkingDays++;
 
-            int empType = checkAttendance();
-            int dailyHours = getDailyWorkingHours(empType);
+            int empType = (int) (Math.floor(Math.random() * 10) % 3);
+            int dailyHours = 0;
+
+            switch (empType) {
+                case IS_FULL_TIME:
+                    dailyHours = FULL_DAY_HOURS;
+                    break;
+
+                case IS_PART_TIME:
+                    dailyHours = PART_TIME_HOURS;
+                    break;
+
+                default:
+                    dailyHours = 0;
+            }
 
             totalWorkingHours += dailyHours;
             totalEmployeeWage += dailyHours * wagePerHour;
         }
-    }
 
-    public void displayEmployeeWage() {
-        System.out.println("Total Wage for " + companyName + " : " + totalEmployeeWage);
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Employee Wage Computation Program");
-
-        EmployeeWageComputation_OOPS tcs = new EmployeeWageComputation_OOPS("TCS", 20, 20, 100);
-
-        EmployeeWageComputation_OOPS infosys = new EmployeeWageComputation_OOPS("Infosys", 25, 22, 110);
-
-        EmployeeWageComputation_OOPS wipro = new EmployeeWageComputation_OOPS("Wipro", 18, 20, 90);
-
-        tcs.computeEmployeeWage();
-        infosys.computeEmployeeWage();
-        wipro.computeEmployeeWage();
-
-        tcs.displayEmployeeWage();
-        infosys.displayEmployeeWage();
-        wipro.displayEmployeeWage();
+        System.out.println(
+                "Total Wage for " + companyName + " : " + totalEmployeeWage
+        );
     }
 }
