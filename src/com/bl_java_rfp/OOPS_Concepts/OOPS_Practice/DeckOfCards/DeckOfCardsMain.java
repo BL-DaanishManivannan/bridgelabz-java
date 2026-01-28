a0acc30cd0926c334c9d07aaf1ef0435ce30a5fc
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 15:53:06 +0530

    Add UC0: Display welcome message for deck of cards program

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/DeckOfCardsMain.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.DeckOfCards;
+
+public class DeckOfCardsMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deck of Cards Program");
+    }
+}
