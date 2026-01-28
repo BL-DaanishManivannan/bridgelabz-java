2ca4b04280b09392239b8485562950cadabe3554
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 14:42:44 +0530

    Edited Main file , with methods and data

--- src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/StockMarket/StockAccountMain.java
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/StockMarket/StockAccountMain.java
@@ -4,5 +4,19 @@ public class StockAccountMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Stock Account Management Program");
+
+        StockPortfolio portfolio = new StockPortfolio();
+
+        Stock tcs = new Stock("TCS", 10, 800);
+        Stock infosys = new Stock("Infosys", 5, 1200);
+        Stock wipro = new Stock("Wipro", 8, 600);
+
+        portfolio.addStock(tcs);
+        portfolio.addStock(infosys);
+        portfolio.addStock(wipro);
+
+        double totalValue = portfolio.calculateTotalPortfolioValue();
+
+        System.out.println("Total Portfolio Value: " + totalValue);
     }
 }
