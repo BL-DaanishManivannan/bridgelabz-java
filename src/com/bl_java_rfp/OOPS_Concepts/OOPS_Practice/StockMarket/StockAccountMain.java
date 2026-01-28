91724f7ebdaac2c3e61ef1b9db9b4b96d4c9618c
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 14:27:51 +0530

    Base StockAccountMain Class initiated

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/StockMarket/StockAccountMain.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.StockMarket;
+
+public class StockAccountMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stock Account Management Program");
+    }
+}
