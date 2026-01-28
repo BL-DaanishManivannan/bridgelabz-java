959a9de746c5d4b57d68030f840606547596a81e
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 11:36:49 +0530

    Initialisation for Address Book

---
+++ src/com/bl_java_rfp/OOPS_Concepts/AddressBook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.OOPS_Concepts.AddressBook;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
