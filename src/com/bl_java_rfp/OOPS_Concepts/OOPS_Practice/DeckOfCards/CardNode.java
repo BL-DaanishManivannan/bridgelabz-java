8fb99fdc1309956a4036dccb1a2306994c0154bc
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 17:31:59 +0530

    Add UC5.3: Store player cards using linked list

---
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/CardNode.java
@@ -0,0 +1,12 @@
+package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.DeckOfCards;
+
+public class CardNode {
+
+    String card;
+    CardNode next;
+
+    public CardNode(String card) {
+        this.card = card;
+        this.next = null;
+    }
+}
--- src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/Player.java
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/Player.java
@@ -3,27 +3,37 @@ package com.bl_java_rfp.OOPS_Concepts.OOPS_Practice.DeckOfCards;
 public class Player {
 
     private String playerName;
-    private String[] cards;
+    private CardNode head;
+    private CardNode tail;
     private int cardCount;
 
-    public Player(String playerName, int maxCards) {
+    public Player(String playerName) {
         this.playerName = playerName;
-        this.cards = new String[maxCards];
+        this.head = null;
+        this.tail = null;
         this.cardCount = 0;
     }
 
+    // UC5.3: Add card to linked list
     public void addCard(String card) {
-        if (cardCount < cards.length) {
-            cards[cardCount++] = card;
+        CardNode newNode = new CardNode(card);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
+        cardCount++;
     }
 
     public String getPlayerName() {
         return playerName;
     }
 
-    public String[] getCards() {
-        return cards;
+    public CardNode getHead() {
+        return head;
     }
 
     public int getCardCount() {
