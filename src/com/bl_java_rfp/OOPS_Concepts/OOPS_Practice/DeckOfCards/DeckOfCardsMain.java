3bb05b8e580dd250624cca57b50daf127603772f
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-28 16:01:01 +0530

    Add UC1: Initialize standard 52-card deck

--- src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/DeckOfCardsMain.java
+++ src/com/bl_java_rfp/OOPS_Concepts/OOPS_Practice/DeckOfCards/DeckOfCardsMain.java
@@ -4,5 +4,22 @@ public class DeckOfCardsMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Deck of Cards Program");
+
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {
+                "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King", "Ace"
+        };
+
+        String[] deck = new String[52];
+        int index = 0;
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck[index++] = rank + " of " + suit;
+            }
+        }
+
+        System.out.println("Deck initialized with " + deck.length + " cards");
     }
 }
