dfd71c5ba3df8db08a7b7f2a9c8482cb0cda207b
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 19:51:28 +0530

    Refactor 1 Added :Generic method to find maximum of 3 values

--- src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
@@ -1,27 +1,16 @@
 package com.bl_java_rfp.OOPS_Concepts.Generics.Maximum;
 public class Maximum {
 
-    // UC1: Integer maximum
-    public static Integer testMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(max) > 0) max = b;
-        if (c.compareTo(max) > 0) max = c;
-        return max;
-    }
+    public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+        T max = a;
 
-    // UC2: Float maximum
-    public static Float testMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) max = b;
-        if (c.compareTo(max) > 0) max = c;
-        return max;
-    }
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
 
-    // UC3: String maximum
-    public static String testMaximum(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) max = b;
-        if (c.compareTo(max) > 0) max = c;
         return max;
     }
 
