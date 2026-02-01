e8d085c7e0ec52e66226ec53ce928938f5959fb1
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 19:47:13 +0530

    UC2 Added :Find Maximum of 3 Float Values

--- src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
@@ -1,6 +1,7 @@
 package com.bl_java_rfp.OOPS_Concepts.Generics.Maximum;
 public class Maximum {
 
+    // UC1: Integer maximum
     public static Integer testMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
 
@@ -14,8 +15,22 @@ public class Maximum {
         return max;
     }
 
+    // UC2: Float maximum
+    public static Float testMaximum(Float a, Float b, Float c) {
+        Float max = a;
+
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Maximum of Integers: " +
-                testMaximum(3, 5, 9));
+        System.out.println("Max Integer: " + testMaximum(3, 5, 9));
+        System.out.println("Max Float: " + testMaximum(3.2f, 5.6f, 4.1f));
     }
 }
