450ad5d259465f65eaa03714a5c7ccf95bc0ae3b
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 19:52:51 +0530

    Refactor 2 Added : Generic class to find maximum of 3 values

--- src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
@@ -1,6 +1,23 @@
 package com.bl_java_rfp.OOPS_Concepts.Generics.Maximum;
-public class Maximum {
+public class Maximum<T extends Comparable<T>> {
 
+    private T a;
+    private T b;
+    private T c;
+
+    // Parameterized constructor
+    public Maximum(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    // Instance method calling static generic method
+    public T testMaximum() {
+        return Maximum.testMaximum(a, b, c);
+    }
+
+    // Static generic method
     public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
         T max = a;
 
@@ -15,9 +32,16 @@ public class Maximum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Max Integer: " + testMaximum(3, 5, 9));
-        System.out.println("Max Float: " + testMaximum(3.2f, 5.6f, 4.1f));
-        System.out.println("Max String: " +
-                testMaximum("Apple", "Peach", "Banana"));
+        Maximum<Integer> intMax =
+                new Maximum<>(3, 5, 9);
+        System.out.println("Max Integer: " + intMax.testMaximum());
+
+        Maximum<Float> floatMax =
+                new Maximum<>(3.2f, 5.6f, 4.1f);
+        System.out.println("Max Float: " + floatMax.testMaximum());
+
+        Maximum<String> stringMax =
+                new Maximum<>("Apple", "Peach", "Banana");
+        System.out.println("Max String: " + stringMax.testMaximum());
     }
 }
