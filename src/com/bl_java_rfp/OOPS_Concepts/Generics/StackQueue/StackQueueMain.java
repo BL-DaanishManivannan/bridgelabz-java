0d3b662e69d025c8e2130038db61075bc4b8a235
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 22:35:24 +0530

    UC0 Added : Initialize Stack and Queue project using Generics

---
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/StackQueue/StackQueueMain.java
@@ -0,0 +1,11 @@
+package com.bl_java_rfp.OOPS_Concepts.Generics.StackQueue;
+
+// Project to implement Stack and Queue using LinkedList
+// Uses Java Generics to support multiple data types
+// Forms the base structure for Stack and Queue operations
+public class StackQueueMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack and Queue using Java Generics");
+    }
+}
