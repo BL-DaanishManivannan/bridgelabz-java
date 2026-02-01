f1d6bfbc460d1f1f09504661980b795f87e96385
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 19:33:36 +0530

    UC0: Initialize Find Maximum problem with base class and package structure

---
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
@@ -0,0 +1,7 @@
+package com.bl_java_rfp.OOPS_Concepts.Generics.Maximum;
+
+public class Maximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Problem using Generics");
+    }
+}
