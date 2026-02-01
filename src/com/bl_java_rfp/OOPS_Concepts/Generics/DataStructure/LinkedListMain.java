ccedee4809f6d1cf240add4b5a947172793f82f1
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 21:47:56 +0530

    UC0 Added : Project initialization for Data Structure using Generics

---
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/DataStructure/LinkedListMain.java
@@ -0,0 +1,11 @@
+package com.bl_java_rfp.OOPS_Concepts.Generics.DataStructure;
+
+// Project to implement data structure problems using Java Generics
+// Covers LinkedList, Stack, Queue, and Hash Table concepts
+// Designed using generic and reusable components
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure Problems using Java Generics");
+    }
+}
