c53d9f57ad712dc5c60a905250ff6322695147c8
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 19:48:47 +0530

    UC3 Added :Find Maximum of 3 String Values

--- src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
@@ -4,33 +4,31 @@ public class Maximum {
     // UC1: Integer maximum
     public static Integer testMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
-
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
         return max;
     }
 
     // UC2: Float maximum
     public static Float testMaximum(Float a, Float b, Float c) {
         Float max = a;
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
+        return max;
+    }
 
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-
+    // UC3: String maximum
+    public static String testMaximum(String a, String b, String c) {
+        String max = a;
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
         return max;
     }
 
     public static void main(String[] args) {
         System.out.println("Max Integer: " + testMaximum(3, 5, 9));
         System.out.println("Max Float: " + testMaximum(3.2f, 5.6f, 4.1f));
+        System.out.println("Max String: " +
+                testMaximum("Apple", "Peach", "Banana"));
     }
 }
