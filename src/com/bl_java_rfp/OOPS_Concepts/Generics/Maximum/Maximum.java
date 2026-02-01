7d587761541f4067386c62e373b7ec5586bbcca3
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-01 19:40:29 +0530

    UC1 Added :Find Maximum of 3 Integers

--- src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/Maximum/Maximum.java
@@ -1,7 +1,21 @@
 package com.bl_java_rfp.OOPS_Concepts.Generics.Maximum;
-
 public class Maximum {
+
+    public static Integer testMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Find Maximum Problem using Generics");
+        System.out.println("Maximum of Integers: " +
+                testMaximum(3, 5, 9));
     }
 }
