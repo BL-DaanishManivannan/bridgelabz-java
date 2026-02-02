99b7aa7de2f3e4df4ab03515c78e65e65d46354d
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-02 11:21:47 +0530

    UC0 Added : Initialize HashTable and Binary Search Tree project

---
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/HashTableBST/HashTableBSTMain.java
@@ -0,0 +1,11 @@
+package com.bl_java_rfp.OOPS_Concepts.Generics.HashTableBST;
+
+// Project to implement Hash Table and Binary Search Tree using Java Generics
+// Covers word frequency using HashTable and node operations using BST
+// Acts as base initialization for HashTable and BST problems
+public class HashTableBSTMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to HashTable and Binary Search Tree using Java Generics");
+    }
+}
