70d1c841edc9ae9970f3c08586503322ea850ec6
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-02 11:38:03 +0530

    UC0 Added : Initialize Binary Search Tree project

---
+++ src/com/bl_java_rfp/OOPS_Concepts/Generics/BinarySearchTree/BinarySearchTreeMain.java
@@ -0,0 +1,11 @@
+package com.bl_java_rfp.OOPS_Concepts.Generics.BinarySearchTree;
+
+// Project to implement Binary Search Tree using Java Generics
+// Covers node insertion, size calculation, and search operations
+// Acts as initialization for BST problem statements
+public class BinarySearchTreeMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Binary Search Tree using Java Generics");
+    }
+}
