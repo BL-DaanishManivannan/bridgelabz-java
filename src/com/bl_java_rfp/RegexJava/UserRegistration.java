4f5870ae806c62d19c62be4fa4aa7645cee0b8a0
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-03 15:56:19 +0530

    Added UC0 – User Registration setup

---
+++ src/com/bl_java_rfp/RegexJava/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.RegexJava;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration System");
+    }
+}
