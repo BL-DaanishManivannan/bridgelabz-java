b2d602f7a2a4a02b281939f1e2a486119df8880c
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-04 10:54:51 +0530

    UC2 Added : Validate last name using regex

--- src/com/bl_java_rfp/RegexJava/UserRegistrationValidator.java
+++ src/com/bl_java_rfp/RegexJava/UserRegistrationValidator.java
@@ -3,14 +3,20 @@ package com.bl_java_rfp.RegexJava;
 import java.util.regex.Pattern;
 
 /**
- * UC1 - First Name Validation
- * Validates first name using regex rules.
+ * UC1 & UC2 - Name Validation
+ * Validates first name and last name using regex rules.
  */
 public class UserRegistrationValidator {
 
     private static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{2,}$";
+    private static final String LAST_NAME_REGEX  = "^[A-Z][a-z]{2,}$";
 
     public boolean validateFirstName(String firstName) {
         return Pattern.matches(FIRST_NAME_REGEX, firstName);
+    }
+
+    // UC2: Last name validation
+    public boolean validateLastName(String lastName) {
+        return Pattern.matches(LAST_NAME_REGEX, lastName);
     }
 }
