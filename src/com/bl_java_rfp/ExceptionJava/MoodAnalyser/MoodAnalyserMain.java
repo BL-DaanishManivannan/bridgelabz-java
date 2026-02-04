2be007315aa96e29c6c258eb1ed765f06bd8173d
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-04 22:05:54 +0530

    UC0 Added : Initialize Mood Analyser application with sub-package

---
+++ src/com/bl_java_rfp/ExceptionJava/MoodAnalyser/MoodAnalyserMain.java
@@ -0,0 +1,13 @@
+package com.bl_java_rfp.ExceptionJava.MoodAnalyser;
+
+/**
+ * UC0 - Initialization
+ * Entry point for Mood Analyser application.
+ * Contains only startup logic.
+ */
+public class MoodAnalyserMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser Program");
+    }
+}
