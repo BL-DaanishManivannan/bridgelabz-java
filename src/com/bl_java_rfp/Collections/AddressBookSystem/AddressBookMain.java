1fda0813f35df16e2316824885b084fb84eddc4d
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-07 11:28:55 +0530

    UC0 Added : Display welcome message for Address Book System

---
+++ src/com/bl_java_rfp/Collections/AddressBookSystem/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.bl_java_rfp.Collections.AddressBookSystem;
+
+/**
+ * UC0 - Initialization
+ * Entry point for Address Book System.
+ */
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
