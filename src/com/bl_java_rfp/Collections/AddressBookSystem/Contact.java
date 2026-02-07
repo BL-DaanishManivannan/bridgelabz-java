b5125e9765cd2162d12e5cb1d7f66906ec8392a1
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-07 11:30:41 +0530

    UC1 Added : Create Contact with personal details

---
+++ src/com/bl_java_rfp/Collections/AddressBookSystem/Contact.java
@@ -0,0 +1,53 @@
+package com.bl_java_rfp.Collections.AddressBookSystem;
+
+/**
+ * UC1 - Contact
+ * Represents a person’s contact details.
+ */
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName,
+                   String address, String city, String state,
+                   String zip, String phoneNumber, String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact {" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
