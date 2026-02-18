b132f77691f2e8856e980135e2fe17f7e8b52114
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-18 17:22:28 +0530

    UC1.1 Added : Math operations using functional interface and lambda

--- src/com/bl_java_rfp/LambdaExp/LambdaExpressionMain.java
+++ src/com/bl_java_rfp/LambdaExp/LambdaExpressionMain.java
@@ -1,7 +1,7 @@
 package com.bl_java_rfp.LambdaExp;
+
 /**
- * UC0 - Initialization
- * Entry point for Java 8 Lambda Expressions assignment.
+ * UC0 & UC1.1 - Lambda Expression Main
  */
 public class LambdaExpressionMain {
 
@@ -9,5 +9,20 @@ public class LambdaExpressionMain {
 
         System.out.println("Welcome to Java 8 Lambda Expressions Program");
 
+        // Lambda for Addition
+        MathOperation addition = (a, b) -> a + b;
+
+        // Lambda for Subtraction
+        MathOperation subtraction = (a, b) -> a - b;
+
+        // Lambda for Division
+        MathOperation division = (a, b) -> a / b;
+
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("Addition: " + addition.operate(num1, num2));
+        System.out.println("Subtraction: " + subtraction.operate(num1, num2));
+        System.out.println("Division: " + division.operate(num1, num2));
     }
 }
---
+++ src/com/bl_java_rfp/LambdaExp/MathOperation.java
@@ -0,0 +1,10 @@
+package com.bl_java_rfp.LambdaExp;
+
+/**
+ * UC1.1 - Functional Interface
+ * Represents a math operation.
+ */
+public interface MathOperation {
+
+    int operate(int a, int b);
+}
