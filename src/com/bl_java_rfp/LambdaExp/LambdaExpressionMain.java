b0cb71bb18342f93c89f37bcdd68ca4de668430e
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-18 15:14:57 +0530

    UC0 Added : Initialize Lambda Expressions assignment

---
+++ src/com/bl_java_rfp/LambdaExp/LambdaExpressionMain.java
@@ -0,0 +1,13 @@
+package com.bl_java_rfp.LambdaExp;
+/**
+ * UC0 - Initialization
+ * Entry point for Java 8 Lambda Expressions assignment.
+ */
+public class LambdaExpressionMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java 8 Lambda Expressions Program");
+
+    }
+}
