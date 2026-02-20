893485abdf05c77cd73eb508622b524269662e2b
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-20 12:48:15 +0530

    UC1.2 Added : Pass lambda as parameter to print result

--- src/com/bl_java_rfp/LambdaExp/LambdaExpressionMain.java
+++ src/com/bl_java_rfp/LambdaExp/LambdaExpressionMain.java
@@ -1,7 +1,7 @@
 package com.bl_java_rfp.LambdaExp;
 
 /**
- * UC0 & UC1.1 - Lambda Expression Main
+ * UC0 → UC1.2
  */
 public class LambdaExpressionMain {
 
@@ -9,20 +9,16 @@ public class LambdaExpressionMain {
 
         System.out.println("Welcome to Java 8 Lambda Expressions Program");
 
-        // Lambda for Addition
         MathOperation addition = (a, b) -> a + b;
-
-        // Lambda for Subtraction
         MathOperation subtraction = (a, b) -> a - b;
-
-        // Lambda for Division
         MathOperation division = (a, b) -> a / b;
 
         int num1 = 10;
         int num2 = 5;
 
-        System.out.println("Addition: " + addition.operate(num1, num2));
-        System.out.println("Subtraction: " + subtraction.operate(num1, num2));
-        System.out.println("Division: " + division.operate(num1, num2));
+        // UC1.2: Passing lambda as parameter
+        MathOperation.printResult(num1, num2, "Addition", addition);
+        MathOperation.printResult(num1, num2, "Subtraction", subtraction);
+        MathOperation.printResult(num1, num2, "Division", division);
     }
 }
--- src/com/bl_java_rfp/LambdaExp/MathOperation.java
+++ src/com/bl_java_rfp/LambdaExp/MathOperation.java
@@ -1,10 +1,15 @@
 package com.bl_java_rfp.LambdaExp;
 
 /**
- * UC1.1 - Functional Interface
- * Represents a math operation.
+ * UC1.1 & UC1.2 - Functional Interface
  */
+@FunctionalInterface
 public interface MathOperation {
 
     int operate(int a, int b);
+
+    // UC1.2: Static method to print result
+    static void printResult(int a, int b, String operation, MathOperation op) {
+        System.out.println("Result of " + operation + " : " + op.operate(a, b));
+    }
 }
