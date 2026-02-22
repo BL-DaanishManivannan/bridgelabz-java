eb802ecfc7cb2d2fa53c9b30ee3df8b711c0b52e
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-02-22 19:54:40 +0530

    UC0 Added : Initialize Streams assignment with base list

---
+++ src/com/bl_java_rfp/Java8/Streams/StreamApiMain.java
@@ -0,0 +1,21 @@
+package com.bl_java_rfp.Java8.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * UC0 - Initialization
+ * Entry point for Java 8 Streams assignment.
+ */
+public class StreamApiMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java 8 Streams API Program");
+
+        // Base List of integers
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        System.out.println("Initial Numbers: " + numbers);
+    }
+}
