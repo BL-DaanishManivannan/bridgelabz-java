89bad82681a00d191bbf7c0219364e6e4b108956
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-03-16 15:25:52 +0530

    UC0 Added : Initialized AddressBookMain with welcome message

---
+++ src/com/bl_java_rfp/FileIO/AddressBook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.FileIO.AddressBook;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
