50a28f851be4a8dd7d740eff26d99adbcc9f2bd2
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-03-16 13:45:26 +0530

    UC0 Added : Initialized HotelReservation with welcome message

---
+++ src/com/bl_java_rfp/AdvanceJava/HotelReservation/HotelReservation.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.AdvanceJava.HotelReservation;
+
+public class HotelReservation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
