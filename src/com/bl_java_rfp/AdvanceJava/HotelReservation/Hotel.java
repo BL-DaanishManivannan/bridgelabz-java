3be18ca87714552436dc6e93fc6a5aab2ee09e24
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-03-16 14:29:43 +0530

    UC5 Added : Added rating field to Hotel class

--- src/com/bl_java_rfp/AdvanceJava/HotelReservation/Hotel.java
+++ src/com/bl_java_rfp/AdvanceJava/HotelReservation/Hotel.java
@@ -5,11 +5,13 @@ public class Hotel {
     private String name;
     private int regularWeekdayRate;
     private int regularWeekendRate;
+    private int rating;
 
-    public Hotel(String name, int regularWeekdayRate, int regularWeekendRate) {
+    public Hotel(String name, int regularWeekdayRate, int regularWeekendRate, int rating) {
         this.name = name;
         this.regularWeekdayRate = regularWeekdayRate;
         this.regularWeekendRate = regularWeekendRate;
+        this.rating = rating;
     }
 
     public String getName() {
@@ -24,10 +26,15 @@ public class Hotel {
         return regularWeekendRate;
     }
 
+    public int getRating() {
+        return rating;
+    }
+
     @Override
     public String toString() {
         return "Hotel: " + name
                 + " | Weekday Rate: $" + regularWeekdayRate
-                + " | Weekend Rate: $" + regularWeekendRate;
+                + " | Weekend Rate: $" + regularWeekendRate
+                + " | Rating: " + rating;
     }
 }
--- src/com/bl_java_rfp/AdvanceJava/HotelReservation/HotelReservation.java
+++ src/com/bl_java_rfp/AdvanceJava/HotelReservation/HotelReservation.java
@@ -8,9 +8,9 @@ public class HotelReservation {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
 
-        Hotel lakewood   = new Hotel("Lakewood", 110, 90);
-        Hotel bridgewood = new Hotel("Bridgewood", 160, 60);
-        Hotel ridgewood  = new Hotel("Ridgewood", 220, 150);
+        Hotel lakewood   = new Hotel("Lakewood",   110, 90,  3);
+        Hotel bridgewood = new Hotel("Bridgewood", 160, 60,  4);
+        Hotel ridgewood  = new Hotel("Ridgewood",  220, 150, 5);
 
         List<Hotel> hotelList = Arrays.asList(lakewood, bridgewood, ridgewood);
         HotelService hotelService = new HotelService(hotelList);
@@ -25,6 +25,7 @@ public class HotelReservation {
         int totalRate  = hotelService.calculateTotalRate(cheapest, weekdays, weekends);
 
         System.out.println("Cheapest Hotel: " + cheapest.getName()
-                + ", Total Rates: $" + totalRate);
+                + " | Rating: " + cheapest.getRating()
+                + " | Total Rates: $" + totalRate);
     }
 }
