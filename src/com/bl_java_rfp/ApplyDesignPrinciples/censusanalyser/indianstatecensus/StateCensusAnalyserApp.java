17d26878b1e6aa51e36aa337f54820db50bcf741
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-03-24 11:37:50 +0530

    UC0 Updated : Package name changed to ApplyDesignPrinciples

---
+++ src/com/bl_java_rfp/ApplyDesignPrinciples/censusanalyser/indianstatecensus/StateCensusAnalyserApp.java
@@ -0,0 +1,8 @@
+package com.bl_java_rfp.ApplyDesignPrinciples.censusanalyser.indianstatecensus;
+
+public class StateCensusAnalyserApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Analyser");
+    }
+}
