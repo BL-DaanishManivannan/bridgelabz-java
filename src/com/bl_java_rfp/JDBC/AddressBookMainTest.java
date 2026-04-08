ed14d1ba7b821f18067205623f1f6e922985e451
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-04-08 12:49:33 +0530

    UC4 Added : Delete person and consolidated AddressBookTest till UC4

--- src/com/bl_java_rfp/JDBC/AddressBookMainTest.java
+++ src/com/bl_java_rfp/JDBC/AddressBookMainTest.java
@@ -1,23 +1,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AddressBookTest {
+class AddressBookMainTest {
 
     @Test
-    void givenPerson_whenEdited_shouldUpdateDetails() {
-        AddressBook addressBook = new AddressBook();
-
-        Person person = new Person(
-                "Daanish", "M",
-                "Old Street", "Chennai",
-                "TN", "600001",
-                "9999999999"
-        );
-
-        addressBook.addPerson(person);
-
-        addressBook.editPerson("Daanish", "New Street");
-
-        assertEquals("New Street", person.getAddress());
+    void givenProgramStarts_shouldDisplayWelcomeMessage() {
+        AddressBookMain main = new AddressBookMain();
+        String message = main.getWelcomeMessage();
+        assertEquals("Welcome to Address Book Program", message);
     }
+
+
 }
---
+++ src/com/bl_java_rfp/JDBC/AddressBookTest.java
@@ -0,0 +1,82 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookTest {
+
+    // ✅ UC0 - Welcome Message
+    @Test
+    void givenProgramStarts_shouldDisplayWelcomeMessage() {
+        AddressBookMain main = new AddressBookMain();
+        String message = main.getWelcomeMessage();
+        assertEquals("Welcome to Address Book Program", message);
+    }
+
+    // ✅ UC1 - Create Person
+    @Test
+    void givenPersonDetails_shouldCreatePersonObject() {
+        Person person = new Person(
+                "Daanish", "M",
+                "Street 1", "Chennai",
+                "TN", "600001",
+                "9999999999"
+        );
+
+        assertEquals("Daanish", person.getFirstName());
+        assertEquals("M", person.getLastName());
+        assertEquals("Chennai", person.getCity());
+    }
+
+    // ✅ UC2 - Add Person
+    @Test
+    void givenPerson_shouldBeAddedToAddressBook() {
+        AddressBook addressBook = new AddressBook();
+
+        Person person = new Person(
+                "Daanish", "M",
+                "Street 1", "Chennai",
+                "TN", "600001",
+                "9999999999"
+        );
+
+        addressBook.addPerson(person);
+
+        assertEquals(1, addressBook.getPersonCount());
+    }
+
+    // ✅ UC3 - Edit Person
+    @Test
+    void givenPerson_whenEdited_shouldUpdateDetails() {
+        AddressBook addressBook = new AddressBook();
+
+        Person person = new Person(
+                "Daanish", "M",
+                "Old Street", "Chennai",
+                "TN", "600001",
+                "9999999999"
+        );
+
+        addressBook.addPerson(person);
+
+        addressBook.editPerson("Daanish", "New Street");
+
+        assertEquals("New Street", person.getAddress());
+    }
+
+    // ✅ UC4 - Delete Person
+    @Test
+    void givenPerson_whenDeleted_shouldRemoveFromAddressBook() {
+        AddressBook addressBook = new AddressBook();
+
+        Person person = new Person(
+                "Daanish", "M",
+                "Street 1", "Chennai",
+                "TN", "600001",
+                "9999999999"
+        );
+
+        addressBook.addPerson(person);
+        addressBook.deletePerson("Daanish");
+
+        assertEquals(0, addressBook.getPersonCount());
+    }
+}
